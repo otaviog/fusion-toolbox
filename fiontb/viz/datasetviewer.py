b7629203927a2a47bd3bb78292889125ea109dd2
Otavio Gomes <otavio.b.gomes@gmail.com>
2019-03-08 14:41:36 -0300

    Updated viewer for correctly use of the new shapelab's camera node.

--- fiontb/viz/datasetviewer.py
+++ fiontb/viz/datasetviewer.py
@@ -6,7 +6,6 @@ from collections import deque
 import cv2
 import numpy as np
 from matplotlib.pyplot import get_cmap
-from tqdm import tqdm
 
 import shapelab
 import shapelab.io
@@ -18,11 +17,16 @@ class _Viewer:
         self.view = self.ctx.viewer()
 
 
-_CAM_MATRIX = np.eye(4)
-_CAM_MATRIX[2, 2] = -1
+_CAM_HAND_MATRIX = np.eye(4)
+_CAM_HAND_MATRIX[2, 2] = -1
 
 
 class DatasetViewer:
+    """Viewer of RGB-D datasets. It shows the image, depth, camera point
+    cloud and accumulated world points.
+
+    """
+
     def __init__(self, dataset, title="Dataset"):
         self.dataset = dataset
         self.title = title
@@ -34,6 +38,7 @@ class DatasetViewer:
 
         self.viewer_world = _Viewer(640, 480)
         self.viewer_world.view.set_title("{}: world space".format(title))
+        self.viewer_world.ctx.add_axis_grid(-10, 10, 1)
         self.visited_idxs = set()
 
         self.pcl_deque = deque()
@@ -41,16 +46,15 @@ class DatasetViewer:
 
     def _update_world(self, idx, snap, cam_space, cam_proj):
         if idx in self.visited_idxs:
-            return None
+            return
 
         rt_cam = snap.rt_cam.matrix
-        world_space = np.matmul(
-            np.matmul(_CAM_MATRIX, rt_cam), cam_space)
+        world_space = np.matmul(rt_cam, cam_space)
         pcl = self.viewer_world.ctx.add_point_cloud(
             world_space[:, 0:3], snap.colors)
 
         cam = self.viewer_world.ctx.add_camera(
-            cam_proj, np.matmul(_CAM_MATRIX, rt_cam))
+            cam_proj, np.matmul(rt_cam, _CAM_HAND_MATRIX))
 
         self.pcl_deque.append((pcl, cam))
         self.visited_idxs.add(idx)
@@ -89,7 +93,7 @@ class DatasetViewer:
             cam_space = snap.get_cam_points()
             cam_space = np.insert(cam_space, 3, 1.0, axis=1)
 
-            _cam_matrix_inv_y = _CAM_MATRIX.copy()
+            _cam_matrix_inv_y = _CAM_HAND_MATRIX.copy()
             _cam_matrix_inv_y[1, 1] *= -1
 
             self.viewer_cam.ctx.add_point_cloud(
@@ -117,6 +121,11 @@ class DatasetViewer:
         cv2.imshow(self.title, canvas)
 
     def run(self):
+        """Show the viewer and block until user exit. Keys:
+        * 'q': quit
+        * 'm': toggle masking (when the dataset has it)
+        """
+
         cv2.namedWindow(self.title, cv2.WINDOW_NORMAL)
         cv2.createTrackbar("pos", self.title, 0, len(
             self.dataset) - 1, self._update_canvas)
@@ -138,34 +147,6 @@ class DatasetViewer:
                 break
             elif key == 'm':
                 self.show_mask = not self.show_mask
-
-
-class WorldPointsViewer:
-    def __init__(self, dataset, idx_skip=1, max_frames=None):
-        self.dataset = dataset
-        self.idx_skip = idx_skip
-        if max_frames is not None:
-            self._max_frames = min(max_frames, len(dataset))
-        else:
-            self._max_frames = len(dataset)
-
-    def run(self):
-        context = shapelab.RenderContext.create_default(
-            shapelab.RenderConfig(640, 480))
-
-        point_list = []
-        color_list = []
-        nodes = []
-        for idx in tqdm(range(0, self._max_frames, self.idx_skip),
-                        desc="Loading point-clouds"):
-            snap = self.dataset[idx]
-            points = snap.get_world_points().astype(np.float32)
-
-            context.add_point_cloud(points, snap.colors/255)
-
-        viewer = context.viewer()
-        viewer.reset_view()
-        viewer.wait_key()
 
 
 def _main():
