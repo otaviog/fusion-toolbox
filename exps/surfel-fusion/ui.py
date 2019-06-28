Fix normal calculation during torch refactor

exps/surfel-fusion/ui.py
@@ -142,8 +142,7 @@ class MainLoop:
 
                 filtered_depth_image = bilateral_filter_depth_image(
                     frame.depth_image, frame_pcl.depth_mask, depth_scale=0.001)
-                # filtered_depth_image = (
-                #    filtered_depth_image*1000.0).numpy().astype(np.int32)
+                
                 frame_pcl.normals = compute_normals(filtered_depth_image, frame.info,
                                                     frame_pcl.depth_mask)
 

fiontb/data/scenenn.py
@@ -2,19 +2,20 @@
 """
 
 import numpy as np
+import torch
 import onireader
 
 from fiontb.camera import KCamera, RTCamera
 from fiontb.frame import Frame, FrameInfo
 
 
-KINECT2_KCAM = KCamera(np.array([[356.769928, 0.0, 251.563446],
-                                 [0.0, 430.816498, 237.563446],
-                                 [0.0, 0.0, 1.0]]))
+KINECT2_KCAM = KCamera(torch.tensor([[356.769928, 0.0, 251.563446],
+                                     [0.0, 430.816498, 237.563446],
+                                     [0.0, 0.0, 1.0]], dtype=torch.float))
 
-ASUS_KCAM = KCamera(np.array([[544.47329, 0.0, 320],
-                              [0.0, 544.47329, 240],
-                              [0.0, 0.0, 1.0]]))
+ASUS_KCAM = KCamera(torch.tensor([[544.47329, 0.0, 320],
+                                  [0.0, 544.47329, 240],
+                                  [0.0, 0.0, 1.0]], dtype=torch.float))
 
 
 class SceneNN:
@@ -44,6 +45,7 @@ class SceneNN:
         self.ni_dev = None
         self.ni_dev = onireader.Device()
         self.ni_dev.open(str(self._oni_filepath))
+        self.ni_dev.start()
 
     def _getnext_pair(self):
         depth_img, depth_ts, _ = self.ni_dev.readDepth()
@@ -60,7 +62,7 @@ class SceneNN:
 
             diff = abs(rgb_ts - depth_ts)
             print("Skiping rgb {} and depth {}".format(rgb_ts, depth_ts))
-        return depth_img, rgb_img, depth_ts
+        return depth_img.astype(np.int32), rgb_img, depth_ts
 
     def __getitem__(self, idx):
         # pylint: disable=unused-variable
@@ -91,7 +93,8 @@ def load_scenenn(oni_filepath, traj_filepath, k_cam_dev='asus', ground_truth_mod
             for _ in range(4):
                 line = file.readline()
                 curr_entry.append([float(elem) for elem in line.split()])
-            rt_mtx = np.array(curr_entry)  # cam space to world space
+            # cam space to world space
+            rt_mtx = torch.tensor(curr_entry, dtype=torch.float)
 
             assert rt_mtx.shape == (4, 4)
             trajectory.append(rt_mtx)

fiontb/frame.py
@@ -114,8 +114,9 @@ class _DepthImagePointCloud:
         self.depth_mask = depth_image > 0
         self.kcam = finfo.kcam
 
-        xs, ys = torch.meshgrid(torch.arange(depth_image.size(0), dtype=torch.float),
-                                torch.arange(depth_image.size(1), dtype=torch.float))
+        ys, xs = torch.meshgrid(torch.arange(self.depth_image.size(0), dtype=torch.float),
+                                torch.arange(self.depth_image.size(1), dtype=torch.float))
+
         self.image_points = torch.stack(
             [xs, ys, self.depth_image], 2)
 
@@ -137,58 +138,32 @@ class _DepthImagePointCloud:
         return self._points
 
 
-class FramePointCloud:
+class FramePointCloud(_DepthImagePointCloud):
     """A point cloud ordered by image positions.
     """
 
     def __init__(self, frame: Frame):
-        info = frame.info
+        super(FramePointCloud, self).__init__(frame.depth_image, frame.info)
 
-        self.depth_image = (torch.from_numpy(frame.depth_image).float()*info.depth_scale +
-                            info.depth_bias)
-
-        self.depth_mask = torch.from_numpy(frame.depth_image > 0).byte()
         self.fg_mask = self.depth_mask
         if frame.fg_mask is not None:
-            self.fg_mask = torch.logical_and(torch.from_numpy(frame.fg_mask).byte(), self.depth_mask)
-
-        xs, ys = torch.meshgrid(torch.arange(self.depth_image.size(0), dtype=torch.float),
-                                torch.arange(self.depth_image.size(1), dtype=torch.float))
-        self.image_points = torch.stack(
-            [xs, ys, self.depth_image], 2)
+            self.fg_mask = torch.logical_and(
+                torch.from_numpy(frame.fg_mask).byte(), self.depth_mask)
 
         if frame.rgb_image is not None:
             self.colors = torch.from_numpy(frame.rgb_image)
 
-        self.kcam = info.kcam
-        self.rt_cam = info.rt_cam
-        self._points = None
-        self._normals = None
+        self.rt_cam = frame.info.rt_cam
 
+        self._normals = None
         if frame.normal_image is not None:
             self._normals = frame.normal_image
-
-    @property
-    def points(self):
-        """Points in the camera space property.
-
-        Returns: (:obj:`numpy.ndarray`): [Nx3x1] array of points in the camera space.
-        """
-
-        if self._points is None:
-            if self.kcam is None:
-                raise RuntimeError("Frame doesn't have intrinsics camera")
-
-            self._points = self.kcam.backproject(
-                self.image_points.reshape(-1, 3)).reshape(self.image_points.size())
-        return self._points
 
     @property
     def normals(self):
         if self._normals is None:
             self._normals = _calculate_depth_normals(
-                self.points,
-                self.depth_mask.astype(np.uint8))
+                self.points, self.depth_mask)
 
         return self._normals
 

fiontb/fusion/surfel/_raster.py
@@ -120,7 +120,7 @@ class GlobalSurfelRaster:
 
         context.set_clear_color(0, 0, 0, 0)
         context.render(proj_matrix,
-                       torch.from_numpy(rt_cam.opengl_view_cam).float(),
+                       rt_cam.opengl_view_cam,
                        self.framebuffer,
                        [self.render_surfels_prg], width, height)
 

fiontb/viz/datasetviewer.py
@@ -56,8 +56,7 @@ class DatasetViewer:
         world_space = Homogeneous(rt_cam.cam_to_world) @ cam_space
 
         with self.wcontext.current():
-            pcl = tenviz.create_point_cloud(torch.from_numpy(world_space).float(),
-                                            torch.from_numpy(colors).byte())
+            pcl = tenviz.create_point_cloud(world_space, colors)
             self.world_viewer.get_scene().add(pcl)
             vcam = tenviz.create_virtual_camera(
                 cam_proj,
@@ -103,15 +102,14 @@ class DatasetViewer:
         pcl = FramePointCloud(frame).unordered_point_cloud(world_space=False)
         cam_space = pcl.points
 
-        hand_matrix = np.eye(4)
+        hand_matrix = torch.eye(4)
         hand_matrix[2, 2] = -1
         hand_matrix[1, 1] = -1
 
         with self.context.current():
             self.tv_camera_pcl = tenviz.create_point_cloud(
-                torch.from_numpy(Homogeneous(hand_matrix)
-                                 @ cam_space).float(),
-                torch.from_numpy(pcl.colors))
+                Homogeneous(hand_matrix) @ cam_space,
+                pcl.colors)
         self.cam_viewer.get_scene().add(self.tv_camera_pcl)
 
         cam_proj = tenviz.projection_from_kcam(
@@ -171,7 +169,7 @@ class DatasetViewer:
 
                 if key < 0:
                     quit = True
-                
+
                 key = chr(key & 0xff).lower()
 
                 if key == 'q':