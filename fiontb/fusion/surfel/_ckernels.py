f60d508d7bc9a5da6afbb5e88778b087bb229458
Otavio Gomes <otavio.b.gomes@gmail.com>
2019-10-01 11:04:21 -0300

    Fist change on the python source tree

--- fiontb/fusion/surfel/_ckernels.py
+++
@@ -1,5 +0,0 @@
-from fiontb._cfiontb import (surfel_cave_free_space,
-                             surfel_find_mergeable_surfels,
-                             surfel_find_live_to_model_merges,
-                             surfel_find_feat_live_to_model_merges)
-
