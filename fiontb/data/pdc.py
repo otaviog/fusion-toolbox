aee6c43d9c89022d3aa6633981cca2bc43383bcb
Otavio Gomes <otavio.b.gomes@gmail.com>
2019-12-30 20:11:15 -0300

    Add pdc dataset

---
+++ fiontb/data/pdc.py
@@ -0,0 +1,73 @@
+from pathlib import Path
+
+import yaml
+import cv2
+import quaternion
+import numpy as np
+import torch
+
+from fiontb.frame import Frame, FrameInfo
+from fiontb.camera import KCamera, RTCamera
+
+
+class PDCDataset:
+    def __init__(self, base_dir, kcam, depth_scale, pose_dict):
+        self.base_dir = base_dir
+        self.kcam = kcam
+        self.depth_scale = depth_scale
+        self.pose_dict = pose_dict
+        self._keys = list(pose_dict.keys())
+
+    def __getitem__(self, idx):
+        pose = self.pose_dict[self._keys[idx]]
+
+        quat = pose['camera_to_world']['quaternion']
+        qw, qx, qy, qz = quat['w'], quat['x'], quat['y'], quat['z']
+
+        trans = pose['camera_to_world']['translation']
+        tx, ty, tz = trans['x'], trans['y'], trans['z']
+        rot_mtx = quaternion.as_rotation_matrix(
+            np.quaternion(qw, qx, qy, qz))
+
+        cam_mtx = np.eye(4)
+        cam_mtx[0:3, 0:3] = rot_mtx
+        cam_mtx[0:3, 3] = [tx, ty, tz]
+
+        timestamp = pose['timestamp']
+
+        info = FrameInfo(kcam=self.kcam, depth_scale=self.depth_scale,
+                         depth_bias=0.0, rt_cam=RTCamera(cam_mtx),
+                         timestamp=timestamp)
+
+        depth_file = self.base_dir / 'rendered_images' / \
+            pose['depth_image_filename']
+        rgb_file = self.base_dir / 'images' / pose['rgb_image_filename']
+
+        depth_img = cv2.imread(str(depth_file), cv2.IMREAD_ANYDEPTH)
+        rgb_img = cv2.imread(str(rgb_file))
+        rgb_img = cv2.cvtColor(rgb_img, cv2.COLOR_BGR2RGB)
+
+        return Frame(info, depth_img.astype(np.int32), rgb_img)
+
+    def __len__(self):
+        return len(self.pose_dict)
+
+
+def load_pdc(base_dir):
+    base_dir = Path(base_dir)
+    with open(base_dir / "images/pose_data.yaml") as pose_file:
+        pose_dict = yaml.load(pose_file, Loader=yaml.FullLoader)
+
+    with open(base_dir / "images/camera_info.yaml") as kcam_file:
+        kcam_dict = yaml.load(kcam_file, Loader=yaml.FullLoader)
+
+    kcam = KCamera(torch.tensor(
+        kcam_dict['camera_matrix']['data']).reshape(3, 3))
+
+    return PDCDataset(base_dir, kcam, 1.0/1000.0, pose_dict)
+
+
+def write_pdc(base_dir, dataset):
+
+    pose_dict = {}
+    kcam_dict = {}
