1927488d381377a0be79f99b411ab24773946d5d
Otavio Gomes <otavio.b.gomes@gmail.com>
2020-01-18 17:25:21 -0300

    Remove old slamfeat function

--- include/fiontb/slamfeat.hpp
+++ include/fiontb/slamfeat.hpp
@@ -17,18 +17,6 @@ struct SFCrop {
 };
 
 struct SlamFeatOp {
-  static int ExtractPatch(
-      const torch::Tensor &anc_points_, const torch::Tensor &anc_colors_,
-      const torch::Tensor &anc_mask_, const torch::Tensor &anc_cam_to_world_,
-      const torch::Tensor &pos_points_, const torch::Tensor &pos_colors_,
-      const torch::Tensor &pos_mask_, const torch::Tensor &pos_world_to_cam_,
-      const torch::Tensor &pos_cam_to_world_, const torch::Tensor &pos_kcam_,
-      const torch::Tensor &neg_colors_, float point_dist_thresh,
-      torch::Tensor anc_patch_, torch::Tensor anc_d_patch_,
-      torch::Tensor pos_patch_, torch::Tensor pos_d_patch_, torch::Tensor mask_,
-      torch::Tensor neg_patch_, torch::Tensor neg_d_patch_,
-      torch::Tensor hard_negative_);
-
   static int GenerateTriplet(const SFFrame &anch_pcl,
                              const torch::Tensor anch_cam_to_world,
                              const SFFrame &posv_pcl,
